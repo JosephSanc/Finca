11ee0217c72465f5bd274021b68f79f8f3aee5ae
Joseph <josephdsanchez@outlook.com>
2022-04-08 14:02:22 -0400

    Made working register and login pages

---
+++ Finca/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  Finca
+//
+//  Created by Joseph Sanchez on 4/8/22.
+//
+
+import Foundation
+
+struct K {
+    static let registerToLanding = "RegisterToLanding"
+    static let loginToLanding = "LoginToLanding"
+}
--- Finca/Controllers/LoginViewController.swift
+++ Finca/Controllers/LoginViewController.swift
@@ -6,24 +6,30 @@
 //
 
 import UIKit
+import Firebase
 
 class LoginViewController: UIViewController {
-
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    @IBAction func loginBtn(_ sender: UIButton) {
+        if let email = emailTextField.text, let password = passwordTextField.text {
+            Auth.auth().signIn(withEmail: email, password: password) { authResult, error in
+                if let errorMessage = error {
+                    print(errorMessage)
+                } else {
+                    self.performSegue(withIdentifier: K.loginToLanding, sender: self)
+                }
+            }
+        }
     }
-    */
-
+    
 }
